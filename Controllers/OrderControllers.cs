5bb0145b75f024d5bcc664133ea99e3a2172cc74
quangtrantl <quangtltranluuquang@gmail.com>
2024-11-27 16:57:04 +0700

    Initial commit

---
+++ Controllers/OrderControllers.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using OrderService.Models;
+
+[ApiController]
+[Route("api/[controller]")]
+public class OrderController : ControllerBase
+{
+    private static List<Order> orders = new List<Order>();
+
+    [HttpPost]
+    public ActionResult AddOrder(Order order)
+    {
+        orders.Add(order);
+        return Ok(order);
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Order>> GetOrders()
+    {
+        return Ok(orders);
+    }
+	private readonly HttpClient _httpClient;
+
+    public OrderController(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    [HttpGet("user/{userId}")]
+    public async Task<IActionResult> GetUserById(int userId)
+    {
+        var response = await _httpClient.GetAsync($"http://localhost:5000/api/User/{userId}");
+        if (response.IsSuccessStatusCode)
+        {
+            var user = await response.Content.ReadAsStringAsync();
+            return Ok(user);
+        }
+        return NotFound();
+    }
+
+    [HttpGet("book/{bookId}")]
+    public async Task<IActionResult> GetBookById(int bookId)
+    {
+        var response = await _httpClient.GetAsync($"http://localhost:5001/api/Book/{bookId}");
+        if (response.IsSuccessStatusCode)
+        {
+            var book = await response.Content.ReadAsStringAsync();
+            return Ok(book);
+        }
+        return NotFound();
+    }
+
+}
---
+++ Models/Order.cs
@@ -0,0 +1,15 @@
+﻿namespace OrderService.Models;
+
+public class Order
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public int BookId { get; set; }
+public Order(int Id, int UserId, int BookId)
+    {
+        Id = Id;
+        UserId = UserId;
+        BookId = BookId;
+    }
+}
+
